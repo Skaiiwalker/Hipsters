6f78d7490282662c5ad582b245f44ae9812097b0
Skaiiwalker <lbar8299@740s-pl-119.local>
2015-11-19 08:01:40 -0700

    ok

--- src/hipster/model/Book.java
+++ src/hipster/model/Book.java
@@ -2,5 +2,77 @@ package hipster.model;
 
 public class Book 
 {
-
+	private int pageCount;
+	private String author;
+	private String subject;
+	private String title;
+	private double price;
+	
+	public Book()
+	{
+		this.pageCount = 0;
+		this.author = "";
+		this.subject = "";
+		this.title = "";
+		this.price = -0.00;
+	}
+	
+	public Book(int pageCount, String author, String subject, String title, double price)
+	{
+		this.pageCount = pageCount;
+		this.author = author;
+		this.subject = subject;
+		this.title = title;
+		this.price = price;
+	}
+	
+	public int getPageCount()
+	{
+		
+	}
+	
+	public String getAuthor()
+	{
+		
+	}
+	
+	public String getSubject()
+	{
+		
+	}
+	
+	public String getTitle()
+	{
+		
+	}
+	
+	public double getPrice()
+	{
+		
+	}
+	
+	public void setPageCount(int pageCount)
+	{
+		
+	}
+	
+	public void setAuthor(String author)
+	{
+		
+	}
+	
+	public void setSubject(String subject)
+	{
+		
+	}
+	
+	public void setTitle(String title)
+	{
+		
+	}
+	
+	public void setPrice(String price)
+	{
+		
+	}
 }
